aac488a60480eb1606354aa4583f60fbee8ac545
mikesalgo <mikesalgo@gmail.com>
2021-02-23 13:01:05 +0800

    fromHomePC

--- mysql/demo.sql
+++ mysql/demo.sql
@@ -1,8 +1,8 @@
-
 CREATE DATABASE zaradb;
 
 use zaradb;
 
+#=====R51
 create table IF NOT EXISTS user(
 	   username VARCHAR(20) NOT NULL,
 	   pwd VARCHAR(20) NOT NULL,
@@ -12,4 +12,27 @@ create table IF NOT EXISTS user(
 	INSERT INTO user VALUES ('user1', 'pwd1');
 
 	select * from user;
+
+	#=====R52
+
+	#drop table laptop;
+
+	create table IF NOT EXISTS laptop(
+		   laptop_name VARCHAR(20) NOT NULL,
+		   username VARCHAR(20) NOT NULL,
+		   PRIMARY KEY ( laptop_name),
+		   FOREIGN KEY (username) REFERENCES user(username)
+		) ENGINE=INNODB;
+
+		INSERT INTO laptop VALUES ('laptop1', 'user1');
+
+		select * from laptop;
+
+
+		-----
+		drop database if exists tmp_schema;
+		create database tmp_schema;
+
+		use tmp_schema;
+		select * from user;
 
