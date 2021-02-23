fcde0d7901d6ba22edc8f256d736fcab329e789e
mikesalgo <mikesalgo@gmail.com>
2021-02-23 13:22:17 +0800

    fromHomePC

--- mysql/demo.sql
+++ mysql/demo.sql
@@ -1,38 +1,26 @@
-CREATE DATABASE zaradb;
-
-use zaradb;
 
 #=====R51
 create table IF NOT EXISTS user(
-	   username VARCHAR(20) NOT NULL,
-	   pwd VARCHAR(20) NOT NULL,
-	   PRIMARY KEY ( username )
-	) ENGINE=INNODB;
+   username VARCHAR(20) NOT NULL,
+   pwd VARCHAR(20) NOT NULL,
+   PRIMARY KEY ( username )
+) ENGINE=INNODB;
 
-	INSERT INTO user VALUES ('user1', 'pwd1');
+INSERT INTO user VALUES ('user1', 'pwd1');
 
-	select * from user;
+select * from user;
 
-	#=====R52
+#=====R52
 
-	#drop table laptop;
+#drop table laptop;
 
-	create table IF NOT EXISTS laptop(
-		   laptop_name VARCHAR(20) NOT NULL,
-		   username VARCHAR(20) NOT NULL,
-		   PRIMARY KEY ( laptop_name),
-		   FOREIGN KEY (username) REFERENCES user(username)
-		) ENGINE=INNODB;
+create table IF NOT EXISTS laptop(
+   laptop_name VARCHAR(20) NOT NULL,
+   username VARCHAR(20) NOT NULL,
+   PRIMARY KEY ( laptop_name),
+   FOREIGN KEY (username) REFERENCES user(username)
+) ENGINE=INNODB;
 
-		INSERT INTO laptop VALUES ('laptop1', 'user1');
+INSERT INTO laptop VALUES ('laptop1', 'user1');
 
-		select * from laptop;
-
-
-		-----
-		drop database if exists tmp_schema;
-		create database tmp_schema;
-
-		use tmp_schema;
-		select * from user;
-
+select * from laptop;
